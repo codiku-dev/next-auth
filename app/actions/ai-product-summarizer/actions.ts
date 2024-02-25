6717b52c039c53ac5fefbc3e8503cef5a238b686
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2024-02-25 18:05:26 +0100

    wip

--- app/actions/ai-product-summarizer/actions.ts
+++ app/actions/ai-product-summarizer/actions.ts
@@ -1,9 +1,9 @@
 import { safeAction } from "@/libs/request";
-import { ReadProductAnalysisModelParams } from "./models";
-import fs from "fs";
-import path from "path";
 import { ChatOllama } from "@langchain/community/chat_models/ollama";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import fs from "fs";
+import path from "path";
+import { ReadProductAnalysisModelParams } from "./models";
 
 async function readReviewsFile(): Promise<string> {
   const filePath = path.resolve(process.cwd(), "reviews.json");
@@ -16,33 +16,33 @@ async function readReviewsFile(): Promise<string> {
   }
 }
 
-export const readProductAnalysis = safeAction(
-  async ({ productUrl }): Promise<ReviewSummary> => {
-    const reviews = await readReviewsFile();
-    const chatModel = new ChatOllama({
-      baseUrl: "http://localhost:11434", // Default value
-      model: "mistral",
-      temperature: 0.3,
-      cache: false,
-    });
+export const readProductAnalysis = safeAction(async ({ productUrl }): Promise<ReviewSummary> => {
+  const reviews = await readReviewsFile();
+  console.log("The reviews", reviews);
+  const chatModel = new ChatOllama({
+    baseUrl: "http://localhost:11434", // Default value
+    model: "mistral",
+    temperature: 0.3,
+    cache: false,
+  });
 
-    const prompt = ChatPromptTemplate.fromMessages([
-      ["system", `You are an Amazon product review summarizer.`],
-      [
-        "user",
-        `Summarize Amazon reviews highlighting key pros and cons with frequency estimates. {input}. Desired output is JSON, it's important : {{"summary": "", "pros":[{{"description":"","frequency":"X%"}},{{"description":"","frequency":"Y%"}}],"cons":[{{"description":"","frequency":"Z%"}},{{"description":"","frequency":"W%"}}]}}`,
-      ],
-    ]);
-
-    console.log("prompt...");
-    const chain = prompt.pipe(chatModel);
-    console.log("thinking...");
-    const resp = await chain.invoke({
-      input: reviews,
-    });
-    console.log(resp.content);
-    const response = JSON.parse(resp.content as string) as ReviewSummary;
-    return response;
-  },
-  ReadProductAnalysisModelParams
-);
+  const prompt = ChatPromptTemplate.fromMessages([
+    ["system", `You are an Amazon product review summarizer.`],
+    [
+      "user",
+      `Summarize Amazon reviews highlighting key pros and cons with frequency estimates. {input}. Desired output is JSON, it's important : {{"summary": "", "pros":[{{"description":"","frequency":"X%"}},{{"description":"","frequency":"Y%"}}],"cons":[{{"description":"","frequency":"Z%"}},{{"description":"","frequency":"W%"}}]}}`,
+    ],
+  ]);
+  /*
+  console.log("prompt...");
+  const chain = prompt.pipe(chatModel);
+  console.log("thinking...");
+  const resp = await chain.invoke({
+    input: reviews,
+  });
+  console.log(resp.content);
+  const response = JSON.parse(resp.content as string) as ReviewSummary;
+  return response;
+  */
+  return { cons: [], pros: [], summary: "" };
+}, ReadProductAnalysisModelParams);
--- app/api/ai-product-summarizer/route.ts
+++ app/api/ai-product-summarizer/route.ts
@@ -1,13 +1,14 @@
-import { GetProductAnalysisModelQParams } from "./models";
-import { NextRequest, NextResponse } from "next/server";
-import { safeEndPoint } from "@/libs/jwt";
 import { readProductAnalysis } from "@/app/actions/ai-product-summarizer/actions";
+import { safeEndPoint } from "@/libs/jwt";
+import { NextRequest, NextResponse } from "next/server";
+import { GetProductAnalysisModelQParams } from "./models";
 
 export const GET = safeEndPoint(
   async (_req: NextRequest, _, __, { productUrl }) => {
     try {
-      const created = await readProductAnalysis({ productUrl });
-      return NextResponse.json(created);
+      console.log("in ");
+      const created = await readProductAnalysis({ productUrl: "azeaz" });
+      return NextResponse.json({});
     } catch (error) {
       return NextResponse.json({ error }, { status: 500 });
     }
